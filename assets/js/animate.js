93161947f479838db7498e7161c1c43d18bac6c9
Yomikoda <yomiko@Yomiko-Padalecki.local>
2016-11-11 11:31:48 +0100

    Beginning of Header animations + Figurint out the whole nav/burger situation

---
+++ assets/js/animate.js
@@ -0,0 +1,27 @@
+$(function(){
+    
+    $(document).ready(function(){
+        $('header h1').css({'opacity':'1','left':'50%'});
+        $('header h2').css({'opacity':'1','left':'50%'});     
+        $('#mousescroll').css('opacity','1');
+    });
+    
+    
+    
+     $(window).scroll(function(){
+         valeurDuScroll=$(window).scrollTop();
+              
+     
+    if(valeurDuScroll>50){
+    
+       
+            
+    }
+     
+         
+     });
+    
+    
+    
+    
+});
