653d3db303aaec76624013180438344e0e3c4dc8
Yomikoda <yomiko@Yomiko-Padalecki.local>
2016-11-11 21:30:53 +0100

    BurgerNav functionnal- JS AND BURGER DONE /o/

--- assets/js/animate.js
+++ assets/js/animate.js
@@ -1,4 +1,19 @@
 $(function(){
+   
+    
+    $('#burger').on('click', function(){
+         if($('#burger').hasClass('clickedburger')){
+             $('#burger').removeClass('clickedburger');
+             $('#burger').css('border','2px solid black');
+             $('#burgernav').css('top','-160px');
+         }else{
+             $('#burger').addClass('clickedburger');
+             $('#burger').css('border','2px solid white'); 
+             $('#burgernav').css('top','0px');
+         }
+  
+    });
+         
     
     $(document).ready(function(){
         $('header h1').css({'opacity':'1','left':'50%'});
